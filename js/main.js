f6f0647ecd802b615443a290ad38fe3783c3569e
gr402_hudal <dimahudobin05@gmail.com>
2023-02-02 13:44:26 +0700

    start

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
 let eventBus = new Vue()
 
+Vue.component('columns', {
+    props:{
+
+    },
+    template:`
+    <div id="cols">
+        <fill></fill>
+        <p v-if="errors.length"
+        v-for="error in errors">
+            {{ error }}
+        </p>
+        <div class="col">
+            <h2>Stage 1</h2>
+            <card></card>
+        </div>
+        <div class="col">
+            <h2>Stage 2</h2>
+        </div>
+        <div class="col">
+            <h2>Tasks completed</h2>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            col1:[],
+            col2:[],
+            col3:[],
+            errors:[],
+            count:[],
+        }
+    },
+    methods:{
+
+    },
+
+})
+
 Vue.component('fill', {
     props: {
 
@@ -9,41 +47,24 @@ Vue.component('fill', {
         <form @submit.prevent="onSubmit">
             <p> 
                 <b>Title</b>
-                <label>
                 <input required type="text" v-model="title" placeholder="title">
-                </label>
             </p>
             <ul>
                 <li>
-                    <label>
-                    <input required type="text" v-model="t1" placeholder="subtask"> 
-                    </label>
+                    <input required type="text" v-model="t1" placeholder="task"> 
                 </li>
-                
                 <li>
-                    <label>
-                    <input required type="text" v-model="t2" placeholder="subtask">
-                    </label>
+                    <input required type="text" v-model="t2" placeholder="task">
                 </li>
-                    
                 <li>
-                    <label>
-                    <input required type="text" v-model="t3" placeholder="subtask">
-                    </label>
+                    <input required type="text" v-model="t3" placeholder="task">
                 </li>
-                    
                 <li>
-                    <label>
-                    <input type="text" v-model="t4" placeholder="subtask">
-                    </label>
+                    <input type="text" v-model="t4" placeholder="task">
                 </li>
-                    
                 <li >
-                    <label>
-                    <input type="text" v-model="t5" placeholder="subtask">
-                    </label>
+                    <input type="text" v-model="t5" placeholder="task">
                 </li>
-                
                 <p>
                     <input type="submit" value="Add a card">
                 </p>
@@ -66,11 +87,11 @@ Vue.component('fill', {
         onSubmit(){
             let card = {
                 title: this.title,
-                t: [{title: this.t1, completed: false},
-                    {title: this.t2, completed: false},
-                    {title: this.t3, completed: false},
-                    {title: this.t4, completed: false},
-                    {title: this.t5, completed: false}]
+                tasks: [{text: this.t1, completed: false},
+                    {text: this.t2, completed: false},
+                    {text: this.t3, completed: false},
+                    {text: this.t4, completed: false},
+                    {text: this.t5, completed: false}]
             }
             eventBus.$emit('card-submitted', card)
             this.title = null
@@ -79,73 +100,43 @@ Vue.component('fill', {
             this.t3 = null
             this.t4 = null
             this.t5 = null
-            console.log(card)
-        }
+        },
+
     }
 })
 
-Vue.component('columns', {
+Vue.component('card', {
     props:{
         card: {
             title: {
                 type: Text,
                 required: true
             },
-            t: {
+            tasks: {
                 type: Array,
                 required: true,
             }
         },
     },
     template:`
-    <div id="cols">
-        <fill></fill>
-        <p v-if="errors.length"
-        v-for="error in errors">
-            {{ error }}
-        </p>
-        <div class="col">
-            <h2>Stage 1</h2>
-                <div class="card" 
-                v-for="column1 in col1"
-                >
-                    <p>{{ column1.title }}</p>
-                    <label v-for="task in column1.t"
-                    v-if="task.title != null">
-                        <p>
-                            <input type="checkbox" @click="task.completed = true" :disabled="task.completed">
-                            {{ task.title }}
-                        </p>
-                    </label>
-                </div>
+        <div class="card">
+             <p><b>Title: </b>{{ card.title }}</p>
+             <label v-for="task in tasks"
+                    v-if="task.text != null">
+                    <p :class="{ completed:task.completed }">
+                    <input type="checkbox" @click="task.completed" :disabled="task.completed">
+                         {{ task.text }}
+                    </p>
+             </label>
         </div>
-        <div class="col">
-        <h2>Stage 2</h2>
-        {{ col2 }}</div>
-        <div class="col">
-        <h2>Task completed</h2>
-        {{ col3 }}</div>
-    </div>
     `,
     data() {
-        return {
-            col1:[],
-            col2:[],
-            col3:[],
-            errors:[]
+        return{
+
         }
     },
     methods:{
 
-    },
-
-    mounted() {
-        eventBus.$on('card-submitted', card => {
-
-            if (this.col1.length < 3){
-                this.col1.push(card)
-            } else {this.errors.push('You can\'t add more')}
-        })
     }
 })
 
